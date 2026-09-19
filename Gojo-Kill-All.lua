55c38f6eb877abdf3f8cf3b91b132512d619e45f
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-09-19 21:21:54 +0700

    Create Gojo-Kill-All.lua

---
+++ Gojo-Kill-All.lua
@@ -0,0 +1,10 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/houseguynew/jjs-scripts/refs/heads/main/ac%20bypass%20required/gojo%20kill%20all/gojo%20kill%20all%20(base).txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/houseguynew/jjs-scripts/refs/heads/main/ac%20bypass%20required/ac%20bypass.txt"))()
+
+setclipboard("https://discord.gg/2ACZAkcmDP")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "SCRIPT LOADED!",
+    Text = "COPIED DISCORD LINK\n\https://discord.gg/2ACZAkcmDP",
+    Duration = 5
+})
